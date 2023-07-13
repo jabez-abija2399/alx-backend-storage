8a5580a8d907d28e76ac1a7bb7780901c1bbece8
Jabez-abija2399 <jabeznew2399@gmail.com>
2023-07-13 02:12:21 -0700

    5-valid_email.sql file updated

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -1,14 +1,11 @@
--- Initial
-DROP TABLE IF EXISTS users;
-
-CREATE TABLE IF NOT EXISTS users (
-    id int not null AUTO_INCREMENT,
-    email varchar(255) not null,
-    name varchar(255),
-    valid_email boolean not null default 0,
-    PRIMARY KEY (id)
-);
-
-INSERT INTO users (email, name) VALUES ("bob@dylan.com", "Bob");
-INSERT INTO users (email, name, valid_email) VALUES ("sylvie@dylan.com", "Sylvie", 1);
-INSERT INTO users (email, name, valid_email) VALUES ("jeanne@dylan.com", "Jeanne", 1);
+-- SQL script that creates a trigger that resets the attribute valid_email only
+-- when the email has been changed
+DELIMITER |
+CREATE TRIGGER email_bool BEFORE UPDATE ON users
+FOR EACH ROW
+BEGIN
+  IF NEW.email <> OLD.email THEN
+  SET NEW.valid_email = 0;
+  END IF;
+END;
+|
